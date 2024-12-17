dc98a2cffffdab9b49922853f15ac9871fdda9c8
Vitaliy Zlobin <zlovian@yandex.ru>
2024-12-17 10:17:03 +0700

    added project

---
+++ app.py
@@ -0,0 +1,6 @@
+def add(n1, n2):
+    return n1 + n2
+
+
+if __name__ == "__main__":
+    print(add(2, 3))
---
+++ tests.py
@@ -0,0 +1,11 @@
+import unittest
+from app import add
+
+
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEquals(add(2, 3), 5)
+
+
+if __name__ == "__main__":
+    unittest.main()
