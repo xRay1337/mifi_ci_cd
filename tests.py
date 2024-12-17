8dff8402f263239346c1446e6d12da98bef4893f
Vitaliy Zlobin <zlovian@yandex.ru>
2024-12-17 12:06:20 +0700

    changed method unittest in tests.py

--- tests.py
+++ tests.py
@@ -4,8 +4,7 @@ from app import add
 
 class TestApp(unittest.TestCase):
     def test_add(self):
-        pass
-        # self.assertEquals(add(2, 3), 5)
+        self.assertEqual(add(2, 3), 5)
 
 
 if __name__ == "__main__":
