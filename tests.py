f2bd028141e4133cf6d1bb656a3409f57acaf03c
Vitaliy Zlobin <zlovian@yandex.ru>
2024-12-17 12:31:08 +0700

    add test for multiply

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import unittest
-from app import add, minus
+from app import add, minus, multiply
 
 
 class TestApp(unittest.TestCase):
@@ -8,6 +8,9 @@ class TestApp(unittest.TestCase):
 
     def testMinus(self):
         self.assertEqual(minus(2, 3), -1)
+
+    def testMultiply(self):
+        self.assertEqual(multiply(2, 3), 6)
 
 
 if __name__ == "__main__":
